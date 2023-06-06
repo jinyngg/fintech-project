856ff2375d64e4140ed2ca1ebc8894c25da4ee48
jyjang <jinyngg@gmail.com>
2023-06-07 00:46:55 +0900

    init : init project (#1)

---
+++ src/main/kotlin/com/zerobase/fintechproject/FintechProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.zerobase.fintechproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FintechProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<FintechProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/zerobase/fintechproject/FintechProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zerobase.fintechproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FintechProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
