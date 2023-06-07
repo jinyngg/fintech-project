33ff4f0b25ee96dd32440e4fccba9daf077a912e
jyjang <jinyngg@gmail.com>
2023-06-07 20:57:21 +0900

    chore : Swagger 사용을 위한 의존성 추가 (#13)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,7 +8,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
 
-//    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    // Swagger 적용을 위한 의존성 추가
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
 
     implementation(project(":domain"))
 }
