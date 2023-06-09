53109ce412592761dfe95af5f533e5622a8972ee
jyjang <jinyngg@gmail.com>
2023-06-09 13:10:27 +0900

    chore : AOP 의존성 추가 (#21)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,19 +4,22 @@ version = "0.0.1"
 
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(project(":domain"))
 
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
 
-    // Swagger 적용을 위한 의존성 추가
+    // Swagger 의존성 추가
     implementation("io.springfox:springfox-boot-starter:3.0.0")
 
-    implementation(project(":domain"))
+    // Jackson 의존성 추가
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
 
     // test 를 적용하기 위한 mockk 의존성 추가
     testImplementation("io.mockk:mockk:1.12.0")
-
     runtimeOnly("com.h2database:h2")
+    
+    // AOP 의존성 추가
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
 }
